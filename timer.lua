bc6442bdab16d2ccc50fa6406b36da0ee9bd7722
zhoujund <dev.zhoujun@gmail.com>
2015-05-25 11:43:15 +0800

    format

--- timer.lua
+++ timer.lua
@@ -1,62 +1,58 @@
-local ltimer = require "ltimer"
-
-local tiemr_max_id = 4294967295
-local timer_id = 0
-local timer_pool = {}
-local timer = {}
+local ltimer = require("ltimer")
+local timer = {id = 0, max_id = 4294967295, pool = {}}
 
 function timer.cancel(id)
-	if timer_pool[id] then
-		timer_pool[id] = nil
-		ltimer.delete(id)
-	end
+    if timer.pool[id] then
+        timer.pool[id] = nil
+        ltimer.delete(id)
+    end
 end
 
 function timer.cancelall()
-	timer_pool = {}
-	ltimer.deleteall()
+    timer.pool = {}
+    ltimer.deleteall()
 end
 
 local function timeout(elapse_, func_, once_)
-	if timer_id == tiemr_max_id then
-		id = 1
-	else
-		id = timer_id + 1
-	end
-	while timer_pool[id] do
-		id = id + 1
-		if id == timer_id then
-			error("timer id is used up")
-		end
-	end
+    if timer.id == timer.max_id then
+        id = 1
+    else
+        id = timer.id + 1
+    end
+    while timer.pool[id] do
+        id = id + 1
+        if id == timer.id then
+            error("timer id is used up")
+        end
+    end
 
-	timer_id = id
-	timer_pool[id] = {elapse = elapse_, func = func_, once = once_}
-	ltimer.add(id, elapse_)
-	return id
+    timer.id = id
+    timer.pool[id] = {elapse = elapse_, func = func_, once = once_}
+    ltimer.add(id, elapse_)
+    return id
 end
 
 function timer.once(elapse, func)
-	return timeout(elapse, func, true)
+    return timeout(elapse, func, true)
 end
 
 function timer.forever(elapse, func)
-	return timeout(elapse, func, false)
+    return timeout(elapse, func, false)
 end
 
 function timer.expire()
-	ltimer.expire(function(id)
-		info = timer_pool[id]
-		if info then
-			if info["once"] then
-				info["func"]()
-				timer_pool[id] = nil
-			else
-				ltimer.add(id, info["elapse"])
-				info["func"]()
-			end
-		end
-	end)
+    ltimer.expire(function(id)
+        info = timer.pool[id]
+        if info then
+            if info.once then
+                info.func()
+                timer.pool[id] = nil
+            else
+                ltimer.add(id, info.elapse)
+                info.func()
+            end
+        end
+    end)
 end
 
 ltimer.init()
